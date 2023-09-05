8614f9926c79656d1e65f5e75b892e2a66294778
harshalv1 <harshalrathod01@gmail.com>
2023-09-05 14:45:50 +0530

    Update redundant_brackets.py

--- stack/redundant_brackets.py
+++ stack/redundant_brackets.py
@@ -1,29 +1,33 @@
-def find_redundant_brackets(expression):
-    st = []
-    for ch in expression:
+def is_redundant(str):
+    stack = []  # Initialize an empty stack to keep track of characters
+    for i in range(len(str)):
+        ch = str[i]
+
         if ch == "(" or ch == "+" or ch == "-" or ch == "*" or ch == "/":
-            st.append(ch)
-        elif ch == ")":
-            is_redundant = True
-            while len(st) > 0 and st[-1] != "(":
-                if st[-1] == "+" or st[-1] == "-" or st[-1] == "*" or st[-1] == "/":
-                    is_redundant = False
-                st.pop()
-            if len(st) == 0:
-                return True  # There's an unmatched closing bracket
-            st.pop()
-            if is_redundant:
-                return True
-    return False
+            # If the character is an opening parenthesis or an operator, push it onto the stack
+            stack.append(ch)
+        else:
+            if ch == ")":
+                is_redundant = True  # Initialize a flag to check for redundant brackets
 
-expression = "((a+b)+(c-d))"
-print(find_redundant_brackets(expression))  # Output: False
+                top = None  # Initialize 'top' to an arbitrary value
+                while top != "(" and len(stack) > 0:
+                    top = stack.pop()  # Pop the top element from the stack
+                    if top in "+-*/":
+                        is_redundant = False  # If an operator is encountered, it's not redundant
 
-expression = "((a+b))"
-print(find_redundant_brackets(expression))  # Output: True
+                if is_redundant == True:
+                    return True  # If is_redundant is still True, return True to indicate redundant brackets
 
-expression = "(a+b)*(c-d)"
-print(find_redundant_brackets(expression))  # Output: False
+    return False  # If no redundant brackets are found, return False
 
-expression = "(a+b)"
-print(find_redundant_brackets(expression))  # Output: False
+# Test cases
+str1 = '(a+(b)/c)'  # Should return True (redundant brackets around '(b)')
+str2 = '(a+c*b)+(c))'  # Should return True (redundant brackets around '(c))')
+str3 = '(a*b+(c/d))'  # Should return False (no redundant brackets)
+str4 = '((a/b))'  # Should return True (redundant brackets around '((a/b))')
+
+print(is_redundant(str1))
+print(is_redundant(str2))
+print(is_redundant(str3))
+print(is_redundant(str4))
