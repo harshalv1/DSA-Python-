c0df94f9f2b08b410da80f12e4e8244471c13262
harshalv1 <harshalrathod01@gmail.com>
2023-09-06 18:27:30 +0530

    Update revstr.py

--- str/revstr.py
+++ str/revstr.py
@@ -1,13 +1,17 @@
+def reverse(word):
+    
+    reversed_word=""
+    
+    for i in range(len(word)-1,-1,-1):
+        reversed_word=reversed_word+word[i]
+        
+    
+    return reversed_word
+    
+word="pepper"   
+reversed_word=reverse(word)
+print(reversed_word)
+        
 
-def revstr(input_string): 
-    s=0
-    e=len(input_string)-1
 
-    while s<e:
-        input_string[s],input_string[e]=input_string[e],input_string[s] #swapping of every character 
-        s=s+1
-        e=e-1
-
-input_string=list("apple") #the input is a list of characters 
-revstr(input_string) # function call 
-print("Reversed string: ","".join(input_string)) #.join method joins the reversed string with the empty string / "" as a separator 
+        
