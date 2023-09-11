789f76d877d1e90c5773a1288790c6da7146003e
harshalv1 <harshalrathod01@gmail.com>
2023-09-11 13:12:43 +0530

    Update circular_queue.py

--- queue/circular_queue.py
+++ queue/circular_queue.py
@@ -1,4 +1,3 @@
-#implementation of cicular queue 
 class MyCircularQueue:
 
     def __init__(self, k: int):
@@ -7,78 +6,79 @@ class MyCircularQueue:
         self.arr=[0]*k
         self.front=-1
         self.rear=-1
-
-
-    def enQueue(self, value: int) -> bool:
-
-        if self.front==-1: # when the queue is empty 
-            self.front=self.rear=0 #they need to be initialized to the beginning 
-            self.arr[self.rear]=value #value is added 
         
-        elif self.front!=0 and self.rear==self.size-1 or self.rear==(self.front-1)%(self.size-1): #modulo condition is for wrapping around and the entire condition is for queue is full
+    def enQueue(self, value: int) -> bool:
+        
+        if self.isFull():
             return False
         
-        elif self.front!=0 and self.rear==self.size-1: #when front is not at the beginning and rear is at the end 
-            self.rear=0 #so it needs to come to beginning to maintain cyclic nature of the queue 
+        if self.front==-1:
+            self.front=self.rear=0
+            self.arr[self.rear]=value
+        
+        elif self.rear==self.size-1:
+            self.rear=0
             self.arr[self.rear]=value
         
         else:
-            self.rear+=1 #normal push in queue
-            self.arr[self.rear]=value
+            self.rear=self.rear+1
 
-        return True
+        return True    
 
     def deQueue(self) -> bool:
 
-        if self.isEmpty():
+        if self.front==-1:
             return False
         
-        element=self.arr[self.front] #element is stored 
+        ans=self.arr[self.front]
+        self.front=-1
+        if self.front==self.rear:
+            self.front=self.rear=-1
         
-        if self.front==self.rear: # if we are currently on the first and only element 
-            self.front=self.rear=-1 #after popping the queue is empty 
+        elif self.front==self.size-1:
+            self.front=0
+        self.front+=1
 
-        elif self.front==self.size-1: #if we are at the end 
-            self.front=0 # we go to the beginning to maintain cyclic nature 
-        
-        self.front=self.front+1 #normal flow 
-
-        return element
-
-
-
-        
+        return ans
 
     def Front(self) -> int:
-        if self.isEmpty(self):
+        if self.isEmpty():
             return -1
-        
-        return self.arr[self.front]
-        
+        else:
+            return self.arr[self.front]
+
 
     def Rear(self) -> int:
-
-        if self.isEmpty(self):
+        if self.isEmpty():
             return -1
-        
-        return self.arr[self.rear]
+        else:
+            return self.arr[self.rear]      
 
-    
     def isEmpty(self) -> bool:
-        if self.front==-1 and self.rear==-1:
-            return True
+
+        if self.front==-1:
+            return True 
         else:
             return False
         
 
     def isFull(self) -> bool:
 
-        if self.front!=0 and self.rear==self.size-1:
-            return True
-            
-        if self.rear==(self.front-1)%(self.size-1):
+        if (self.rear + 1) % self.size == self.front:
             return True
 
         else:
             return False
+
+
         
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
