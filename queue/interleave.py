7defb7eba0a156d05d0bf4853713470e476fac96
harshalv1 <harshalrathod01@gmail.com>
2023-09-14 12:57:00 +0530

    Create interleave.py

---
+++ queue/interleave.py
@@ -0,0 +1,36 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def rearrangeQueue(self, N : int, q : List[int]) -> List[int]:
+        
+        stack=[]
+        q=deque(q)
+        
+        mid=N//2
+        
+        for i in range(mid):
+            val=q.popleft()
+            stack.append(val)
+        
+        while stack:
+            val=stack.pop()
+            q.append(val)
+            
+        for i in range(mid):
+            val=q.popleft()
+            q.append(val)
+            
+        for i in range(mid):
+            val=q.popleft()
+            stack.append(val)
+            
+        while stack:
+            q.append(stack.pop())
+            val=q.popleft()
+            q.append(val)
+            
+            
+        return list(q)
+        
+        
