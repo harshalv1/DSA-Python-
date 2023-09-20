32b9a02044c8b257229bfcaacd8dfa55a85218f5
harshalv1 <harshalrathod01@gmail.com>
2023-09-20 17:01:03 +0530

    Update bfs.py

--- Trees/bfs.py
+++ Trees/bfs.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,5 +6,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def levelOrder(self, root):
         
+        queue=deque()
+        queue.append(root)
+
+        res=[]
+
+        while queue:
+            level=[]
+            for i in range(len(queue)):
+                node=queue.popleft()
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if level:
+                res.append(level)
+
+        return res
